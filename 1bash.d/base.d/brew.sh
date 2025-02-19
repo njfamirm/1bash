2559436205b63e9b8cc7446116c9f743e1595d70
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2025-02-19 09:01:13 +0330

    fix(bash): improve brew and git completion script checks

--- 1bash.d/base.d/brew.sh
+++ 1bash.d/base.d/brew.sh
@@ -1,3 +1,3 @@
-if command -v /opt/homebrew/bin/brew > /dev/null 2>&1; then
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+if command -v brew >/dev/null 2>&1; then
+    eval "$(brew shellenv)"
 fi
--- 1bash.d/git.sh
+++ 1bash.d/git.sh
@@ -1,8 +1,9 @@
 if [ -f /usr/local/etc/bash_completion ]; then
     . /usr/local/etc/bash_completion
-else
-    # if not found in /usr/local/etc, try the brew --prefix location
-    if [ -f "$(brew --prefix)/etc/bash_completion.d/git-completion.bash" ]; then
-        . "$(brew --prefix)/etc/bash_completion.d/git-completion.bash"
-    fi
+elif [ -f /etc/bash_completion ]; then  # Check system-wide location
+    . /etc/bash_completion
+elif command -v brew &> /dev/null && [ -f "$(brew --prefix)/etc/bash_completion.d/git-completion.bash" ]; then
+    . "$(brew --prefix)/etc/bash_completion.d/git-completion.bash"
+elif [ -f /usr/share/bash-completion/completions/git ]; then
+    . /usr/share/bash-completion/completions/git
 fi
